cfb5485d37195c7bc63668ff261a879365b45fbc
Shlomi <shlomikaufman91@gmail.com>
2026-02-09 21:20:40 +0200

    List of all sensitive ports

--- checks.py
+++ checks.py
@@ -1,6 +1,9 @@
-def list_ip_source(list_data):
+from config import *
+
+
+def list_ip_source(data_list):
     """מחזירה רשימה של כתובות חיצוניות של השולח"""
-    for data in list_data:
+    for data in data_list:
         if is_external(data[1]):
             yield data[1]
 
@@ -8,3 +11,13 @@ def list_ip_source(list_data):
 def is_external(data) -> bool:
     """בודקת האם זה כתובות חיצוניות"""
     return data[0:2:] != "10" and data[0:7:] != "192.168"
+
+
+def port_sensitive_list(data_list):
+    """החזרת רשימה של כל הפורטים הרגישים"""
+    return [data[3] for data in data_list if is_sensitive(data[3])]
+
+ # פונקצית עזר לפורטים
+def is_sensitive(data):
+    """בודקת האם אלה פורטים רגישים"""
+    return data in PORT_SENSITIVE.values()
