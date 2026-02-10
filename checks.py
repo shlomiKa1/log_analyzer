3bf8eafa754d35a95661e5a914edf7c84601f1a4
Shlomi <shlomikaufman91@gmail.com>
2026-02-10 15:07:13 +0200

    add function dict count ip source

--- checks.py
+++ checks.py
@@ -1,12 +1,15 @@
-from config import PORT_SENSITIVE, SIZE_BYTES_FILE
+from operator import countOf
+from pstats import count_calls
+
+from config import PORT_SENSITIVE, SIZE_BYTES_FILE, MAP_ROWS
 
 
 # פונקציה לרשימת IP source
 def list_ip_source(data_list):
     """מחזירה רשימה של כתובות חיצוניות של השולח"""
     for data in data_list:
-        if is_external(data[1]):
-            yield data[1]
+        if is_external(data[MAP_ROWS["IP_SOURCE"]]):
+            yield data[MAP_ROWS["IP_SOURCE"]]
 
 # פונקצית עזר
 def is_external(data) -> bool:
@@ -16,7 +19,7 @@ def is_external(data) -> bool:
 # פונקציה לרישמת פורטים רגישים
 def port_sensitive_list(data_list):
     """החזרת רשימה של כל הפורטים הרגישים"""
-    return [data[3] for data in data_list if is_sensitive(data[3])]
+    return [data[MAP_ROWS["PORT"]] for data in data_list if is_sensitive(data[MAP_ROWS["PORT"]])]
 
  # פונקצית עזר לפורטים
 def is_sensitive(data):
@@ -26,18 +29,16 @@ def is_sensitive(data):
 # פונקציה לרשימת גדלים
 def list_of_size(data_list):
     """החזרת רשימה של הגדלים של הקובץ שיותר מ- 5000"""
-    return (data[-1] for data in data_list if int(data[-1]) > SIZE_BYTES_FILE)
+    return (data[MAP_ROWS["SIZE"]] for data in data_list if int(data[MAP_ROWS["SIZE"]]) > SIZE_BYTES_FILE)
 
 
 # פונקציה תג לגודל קובץ
 def tags_size(data_list):
     """הוספת תג לרשימה רשימות האם יש חריגה או לא"""
-    result = []
+    return [data.append("LARGE") if int(data[MAP_ROWS["SIZE"]]) > SIZE_BYTES_FILE else data.append("NORMAL") for data in data_list]
 
-    for data in data_list:
-        if int(data[-1]) > SIZE_BYTES_FILE:
-            data.append("LARGE")
-        else:
-            data.append("NORMAL")
-        result.append(data)
-    return result
+# stage 2
+def dict_ip_source(data_list):
+    """פונקציה שמחזירה מילון של כתובת IP שולח עם המספר הפניה אליו"""
+    val = [data[MAP_ROWS["IP_SOURCE"]] for data in data_list]
+    return {key: val.count(key) for key in set(val)}
--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+"""
+קובץ להגדרת: משתנים קבועים,
+"""
 
-PORT_SENSITIVE = {'SSH': '22', 'TELNET': '23', 'RDP': '3389'} # מילון של פורטים רגישים
-SIZE_BYTES_FILE = 5000
+
+PORT_SENSITIVE = {"SSH": "22", "TELNET": "23", "RDP": "3389"} # מילון של פורטים רגישים
+SIZE_BYTES_FILE = 5000 # גודל הקובץ
+MAP_ROWS = {"DATE": 0, "IP_SOURCE": 1, "IP_DEST": 2, "PORT": 3, "PROTOCOL": 4, "SIZE": 5, "SIZE_STATUS": 6} # מפה לעמודות
