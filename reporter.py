9c6861dd8ceba3004f6e26cbcda5346836bdeabc
Shlomi <shlomikaufman91@gmail.com>
2026-02-11 15:12:36 +0200

    add function lists of all sensitive port

--- reporter.py
+++ reporter.py
@@ -1,4 +1,4 @@
-from config import KB_FORMAT
+from config import KB_FORMAT, MAP_ROWS, PORT_SENSITIVE
 
 
 def format_time(date_str):
@@ -8,3 +8,7 @@ def format_time(date_str):
 def list_bytes_to_kb(size_str):
     """פונקציה שמקבלת רשימה של מחרזות של גדלים בביטיים ומחזירה רשימה של לקילובייט"""
     return list(map(lambda kb: round(int(kb) / KB_FORMAT, 2), size_str))
+
+def list_of_sensitive_port(data_list):
+    """פונקציה שמקבלת רשימה של רשימות ומחזירה רשימה של רשימותץ של כל ה PORT הרגישים"""
+    return list( filter(lambda port: port[MAP_ROWS["PORT"]] in PORT_SENSITIVE.values(), data_list))
