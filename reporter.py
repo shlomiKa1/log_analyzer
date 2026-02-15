cc3bf2024b9b88c98e25977c1569de98d5cd10de
Shlomi <shlomikaufman91@gmail.com>
2026-02-15 13:09:58 +0200

    add function generate report

--- reporter.py
+++ reporter.py
@@ -1,5 +1,5 @@
-from config import MIN_ONE_SUSPICION, total_lines, total_suspicion_lines, total_of_suspicions
-from analyzer import list_line_checks, dict_of_checks_suspicion, checks_of_all_lines, dict_ip_suspicion
+from config import MIN_ONE_SUSPICION, total_lines, total_suspicion_lines, total_of_suspicions, MIN_THREE_SUSPICION
+from analyzer import list_line_checks, dict_of_checks_suspicion, checks_of_all_lines, dict_ip_suspicion, more_of_tow_suspicion
 from reader import load_csv_on_yield
 
 
@@ -27,6 +27,36 @@ def analyze_log(file):
     """פונקציה המקבלת ניתוב לקובץ ומעבדת  קריאה לקובץ, בדיקת חשודות, מילון עם IP והחשודות ןעדכון סטטיסטיקות"""
     generator_file = list(load_csv_on_yield(file))
     checks_suspicions_line = [list_line_checks(line, dict_of_checks_suspicion()) for line in generator_file]
-    dict_ups = dict_ip_suspicion(generator_file)
+    dict_ips = dict_ip_suspicion(generator_file)
     update_global_statistics(generator_file)
+    return dict_ips
 
+def generate_report(suspicious):
+    """פונקציה המקבלת מילון של IPs עם רשימת החשודות שלה, כך שבונה מחרוזת ל 'דוח תעבורה חשודות'"""
+    report = ""
+    report += "======================================="
+    report += "            דוח תעבורה חשודות          "
+    report += "======================================="
+    report += "\nסטטיסטקות כלליות:\n"
+    report += f" - שורות שנקראו: {total_lines}\n"
+    report += f" - שורות חשודות: {total_suspicion_lines}\n"
+    for key, val in total_of_suspicions.items():
+        report += f" - {key}: {val}\n"
+
+    report += "\nIPs עם רמה סיכון גבוה (3+ חשודות):\n"
+
+    high_risk_level = {}
+    low_risk_level = {}
+    for ips, sus in suspicious.items():
+        if len(sus) >= MIN_THREE_SUSPICION:
+            high_risk_level[ips] = sus
+        else:
+            low_risk_level[ips] = sus
+
+    for ips, sus in high_risk_level.items():
+        report += f" - {ips}: {sus}\n"
+
+    report += "\nIPs חשודים נוספים:"
+    for ips, sus in low_risk_level.items():
+        report += f"\n - {ips}: {sus}"
+    return report
