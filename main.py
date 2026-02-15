a653ddc541099cebcd09e1a09e095075928f2efc
Shlomi <shlomikaufman91@gmail.com>
2026-02-15 13:34:25 +0200

    add main function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from reader import save_report
+from reporter import analyze_log, generate_report
+
+def main():
+    """פונקציה להפעלה התוכנית"""
+    suspicious = analyze_log("network_traffic.log")
+
+    report = generate_report(suspicious)
+
+    print(report)
+
+    save_report(report, "security_report.txt")
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
